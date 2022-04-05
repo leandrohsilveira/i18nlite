3c3418bf28fea0b598dd6359845d9a556c0ebd5c
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2022-04-05 17:47:18 -0300

    Add core module to test run

--- modules/core/jest.config.ts
+++ modules/core/jest.config.ts
@@ -1,7 +1,6 @@
 export default {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
   coverageProvider: 'v8',
   coverageDirectory: 'coverage',
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
